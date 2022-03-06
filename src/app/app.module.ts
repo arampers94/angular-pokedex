edb29ec26b63aadbf83c88e8524a7655c60e563b
Amar <arampers94@gmail.com>
2022-03-06 01:49:43 -0500

    Generated pokemon list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    PokemonListComponent
   ],
   imports: [
     BrowserModule,
