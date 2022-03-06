02d213ea4cf306ef89b29a15f3c8aaefa882cd10
Amar <arampers94@gmail.com>
2022-03-06 01:51:55 -0500

    Generated navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
     PokemonListComponent,
-    ErrorPageComponent
+    ErrorPageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
