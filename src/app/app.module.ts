4c9637532c954836f16065b56d704f3b865afa20
Amar <arampers94@gmail.com>
2022-03-06 01:50:13 -0500

    Generated error page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    PokemonListComponent
+    PokemonListComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
