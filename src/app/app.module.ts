766e0477547c07f0cae1923b1e38d09e893e099f
Amar <arampers94@gmail.com>
2023-10-19 15:36:37 -0400

    added ngrx store and effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { ErrorPageComponent } from './layouts/error-page/error-page.component';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
 import { MainComponent } from './layouts/main/main.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LandingPageModule } from './layouts/landing-page/landing-page.module';
 import { PokemonDetailModule } from './pokemon-detail/pokemon-detail.module';
+import { PokemonListModule } from './pokemon-list/pokemon-list.module';
+import { AppStoreModule } from './store.module';
 
 @NgModule({
   declarations: [
-    PokemonListComponent,
     ErrorPageComponent,
     NavbarComponent,
     MainComponent
@@ -22,7 +22,9 @@ import { PokemonDetailModule } from './pokemon-detail/pokemon-detail.module';
     AppRoutingModule,
     NgbModule,
     LandingPageModule,
-    PokemonDetailModule
+    PokemonDetailModule,
+    PokemonListModule,
+    AppStoreModule,
   ],
   providers: [],
   bootstrap: [MainComponent]
---
+++ src/app/store.module.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { EffectsModule } from "@ngrx/effects";
+import { StoreModule } from "@ngrx/store";
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([])
+  ]
+})
+export class AppStoreModule {}
