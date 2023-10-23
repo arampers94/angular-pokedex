89235a955611d00e440ab15eaeec7055e5a3b78c
Amar <arampers94@gmail.com>
2023-10-23 00:13:36 -0400

    added typeahead to search input

--- src/app/pokedex/pokedex.module.ts
+++ src/app/pokedex/pokedex.module.ts
@@ -4,6 +4,8 @@ import { PokedexComponent } from './pokedex.component';
 import { RegionPickerComponent } from './region-picker/region-picker.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
 
 const ROUTES = [
   {
@@ -21,6 +23,8 @@ const ROUTES = [
   imports: [
     CommonModule,
     RouterModule.forChild(ROUTES),
+    FormsModule,
+    NgbTypeaheadModule,
   ]
 })
 export class PokedexModule { }
--- src/app/pokedex/pokemon-list/pokemon-list.component.ts
+++ src/app/pokedex/pokemon-list/pokemon-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { GameSelectors, LocationSelectors } from '../../store';
+import { Observable, OperatorFunction, debounceTime, distinctUntilChanged, map } from 'rxjs';
+import { Router } from '@angular/router';
+import { NgbTypeaheadSelectItemEvent } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-pokemon-list',
@@ -11,11 +14,35 @@ export class PokemonListComponent implements OnInit {
   public regions$ = this.store$.select(LocationSelectors.selectRegions);
   public currentPokedex = this.store$.select(GameSelectors.selectCurrentPokedexName);
   public pokedex$ = this.store$.select(GameSelectors.selectPokedex);
+  public pokemonName = '';
+  public allPokemon: string[] = [];
 
-  constructor(private store$: Store) { }
+  formatter = (result: string) => result.charAt(0).toUpperCase() + result.slice(1);
+
+  constructor(
+    private store$: Store, 
+    private router: Router, 
+    ) { }
 
   ngOnInit(): void {
-    
+    this.pokedex$.subscribe(pokedex => {
+      if (pokedex) {
+        this.allPokemon = pokedex.pokemon_entries.map(pokemon => pokemon.pokemon_species.name);
+      }
+    });
   }
 
+  search: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) => {
+    return text$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      map((term) =>
+				term.length < 2 ? [] : this.allPokemon.filter((v) => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10),
+      )
+    )
+  }
+
+  searchPokemon(event: NgbTypeaheadSelectItemEvent): void {
+    this.router.navigateByUrl('/pokemon-detail/' + event.item);
+  }
 }
